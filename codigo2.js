05a209b395c9fa1f1a3e230b4768f64df297ebbe
andres <andresazcurra@gmail.com>
2022-02-16 09:14:15 -0300

    ejercicios del laboratorio 2

---
+++ codigo2.js
@@ -0,0 +1,16 @@
+var edadUsuario = window.prompt ("indique su edad")
+alert ("la edad del usuario es : " + edadUsuario )
+
+var nombreUsuario = window.prompt ("indique el nombre del usuario","ponga aqui su nombre")
+alert ("el nombre de ususario es : " + nombreUsuario)
+
+var apellidoUsuario = window.prompt ("indique su apellido", "su apellido va aqui")
+alert ("el apellido de usuario es : " + apellidoUsuario)
+
+
+
+document.write(" <i>El nombre del usuario es : </i>" + nombreUsuario + "<br><i> La edad del ususario es : </i>" + edadUsuario +"<br><i> Y el apellido es </i> " +apellidoUsuario)
+
+
+
+
---
+++ codigo3.js
@@ -0,0 +1,4 @@
+var sueldo = Number(window.prompt("por favor indique cual es su sueldo", "sueldo"))
+var bonus = Number(window.prompt("por favor indique de cuanto es su bono este me ","bono"))
+var resultado = sueldo + bonus
+document.write ("el resusultado total de tu sueldo es " + resultado)
